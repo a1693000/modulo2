43e57755a602b3bf03d7ced494c8d24a90164063
Erick Zen Iwamoto <erick_zen_@hotmail.com>
2019-02-28 17:52:20 -0300

    commit geral

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '172.17.0.2',
+  username: 'erick',
+  password: 'erick',
+  database: 'gonodemodulo2',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
---
+++ src/server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const session = require('express-session')
+const nunjucks = require('nunjucks')
+const path = require('path')
+
+class App {
+  constructor () {
+    this.express = express()
+    this.isDev = process.env.NODE_ENV !== 'production'
+
+    this.middlewares()
+    this.views()
+    this.routes()
+  }
+
+  middlewares () {
+    this.express.use(express.urlencoded({ extended: false }))
+    this.express.use(
+      session({
+        secret: 'MyAppSecret',
+        resave: false,
+        saveUninitialized: false
+      })
+    )
+  }
+
+  views () {
+    nunjucks.configure(path.resolve(__dirname, 'app', 'views'), {
+      watch: this.isDev,
+      express: this.express,
+      autoescape: true
+    })
+
+    this.express.use(express.static(path.resolve(__dirname, 'public')))
+    this.express.set('view engine', 'njk')
+  }
+
+  routes () {
+    this.express.use(require('./routes'))
+  }
+}
+
+module.exports = new App().express
